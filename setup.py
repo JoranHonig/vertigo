4069829e984423cce919e2de260d15397ce635ed
Joran Honig <joran.honig@gmail.com>
2019-08-24 15:48:26 +0200

    Bump version: 1.0.9 → 1.0.10

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [x for x in map(str.strip, REQUIREMENTS.read_text().split("\n")) 
 
 setup(
     name="eth-vertigo",
-    version="1.0.9",
+    version="1.0.10",
     description="Mutation Testing for Ethereum Smart Contracts",
     long_description=README,
     long_description_content_type="text/markdown",
