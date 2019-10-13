05b5114471f4de9519af7ecfcb6da257d0aa6477
Joran Honig <joran.honig@gmail.com>
2019-10-13 16:21:31 +0200

    add base rule loader implementation

--- eth_vertigo/mutator/universal_mutator/rule_loader.py
+++ eth_vertigo/mutator/universal_mutator/rule_loader.py
@@ -1,6 +1,8 @@
 from eth_vertigo.mutator.universal_mutator.rule import Rule
 from pathlib import Path
 from typing import Generator
+from re import compile, error
+from loguru import logger
 
 
 class RuleLoader:
@@ -16,4 +18,30 @@ class RuleLoader:
 
     def load_from_txt(self, rule_txt: str) -> Generator[Rule]:
         """Load mutation rules from text"""
-        pass
+        for line in rule_txt.split("\n"):
+            # Deal with non mutation rule lines
+            if "==>" not in line:
+                if line.startswith("#"):
+                    continue
+                logger.warning(f"Error while parsing Universal Mutator rules:\n{line}")
+                continue
+
+            split_line = line.split("==>")
+            if len(line) < 2:
+                logger.warning(f"Error while parsing Universal Mutator rules:\n{line}")
+                continue
+
+            match = split_line[0]
+            replace = split_line[1]
+
+            # compile regex
+            try:
+                match = compile(match)
+            except error:
+                logger.warning(f"Error while parsing Universal Mutator rules.\nInvalid regular expression:\n{line}")
+
+            # drop return char if present
+            if replace and replace[-1] == '\n':
+                replace = replace[:-1]
+
+            yield Rule(match, replace)
