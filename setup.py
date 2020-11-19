39a1e76cb3027de5b2897d473842c279b230c330
Joran Honig <joran.honig@gmail.com>
2020-11-19 13:19:36 +0100

    Bump version: 1.2.2 → 1.2.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [x for x in map(str.strip, REQUIREMENTS.read_text().split("\n")) 
 
 setup(
     name="eth-vertigo",
-    version="1.2.2",
+    version="1.2.3",
     description="Mutation Testing for Ethereum Smart Contracts",
     long_description=README,
     long_description_content_type="text/markdown",
